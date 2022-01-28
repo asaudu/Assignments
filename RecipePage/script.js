e75f484cf77a8bd8ed8c0628bffef93796c037f1
asaudu <adaoraa.91@gmail.com>
2022-01-28 14:51:45 -0600

    Button change and Media Query

--- RecipePage/script.js
+++ RecipePage/script.js
@@ -1,3 +1,15 @@
 function myAlertBox() {
     alert("Thanks for your submission!");
   }
+
+let colorChange = document.querySelector("body");
+colorChange.style.color = "#A265E2";
+
+  /*document.getElementById('button-id').onclick = changeColor;
+
+  function changeColor() {
+    document.body.style.backgroundColor = "#FFE5B4;
+    return false;*/
+
+    let buttonChange = document.querySelector('#button-id');
+    buttonChange.addEventListener('click', () => buttonChange.style.backgroundColor = 'blue');
