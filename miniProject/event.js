312e74138ab9df77458e850269365623bc0cdba0
asaudu <adaoraa.91@gmail.com>
2022-02-08 09:49:31 -0600

    Updating the JS

--- miniProject/event.js
+++ miniProject/event.js
@@ -1,24 +1,26 @@
 class Event {
     constructor(name, description) {
-        this.name = name;
-        this.description = description;
-        this.availableTickets = [];
-        this.addAvailableTickets = function(ticketName, price) {
-            this.ticketName = ticketName;
-            this.price = price;
-            this.availableTickets.push(ticketName, price);
-        }
-        allTickets() {
-            let allTicketsSummary = "All tickets: ";
-            for (let i = 0; i < this.availableTickets.length; i++) {
-              allTicketsSummary += `${i+1}. ${this.availableTickets[i][0]} ${this.availableTickets[i][1]} `
-              // console.log(allTicketsSummary);
-              // all tickets: 1. human 299 2. vampire 99
-            }
-            return allTicketsSummary;
-          }
-      }
+      this.name = name;
+      this.description = description;
+      this.availableTickets = [];
+    }
+    addAvailableTickets(ticketName, price) {
+      this.ticketName = ticketName;
+      this.price = price;
+      this.availableTickets.push(ticketName, price);
+      //console.log(this.availableTickets);
+    }
+    allTickets(){
+  let ticketSummary = "All tickets";
+  for(let i = 0; i < this.availableTickets.length; i++) {
+    ticketSummary += `${i+1} ${this.availableTickets[i][0]} ($${this.availableTickets[i][1]})`;
+  }
+  return ticketSummary;
 }
+}
+
+
+          
 
 
 let eventObj1 = new Event(
@@ -36,9 +38,16 @@ let eventObj3 = new Event(
     'On the line tour 2019'
 );
 
-
+let eventArray = new Array();
+// how to push mutliple elements at once
 eventArray.push(eventObj1, eventObj2, eventObj3);
+// checking for pushed elements
 console.log(eventArray);
+
+eventObj1.addAvailableTickets("General Admission", 25);
+eventObj1.addAvailableTickets("V.I.P", 35);
+
+console.log(eventObj1.availableTickets);
 
 document.addEventListener('DOMContentLoaded', () => {
     let html = '';
@@ -48,20 +57,24 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#event').innerHTML = html;
   });
 
-
   class TicketType {
-    constructor(name, price) {
-        this.name = name;
-        this.price = price;
-      }
-}
-let eventArray = new Array();
+    constructor(name, description) {
+      this.name = name;
+      this.price = price;
+    }
+  }
 
-let newTicket = new TicketType ("Zenon", "75");
+
+
+//let newTicket = new TicketType ("Zenon", "75")eventObj1.addAvailableTickets("human", 299);
+eventObj1.addAvailableTickets("vampire", 99);
+//console.log(eventObj1);
   
-eventObj2.addAvailableTickets("General Admission", 25)
+eventObj2.addAvailableTickets("General Admission", 25);
 eventObj2.addAvailableTickets("Floor Seating", 80)
+console.log(eventObj2.availableTickets);
 
 eventObj3.addAvailableTickets("Orchestra", 300)
 eventObj3.addAvailableTickets("Mezzanine", 200)
 eventObj3.addAvailableTickets("Balcony", 100)
+console.log(eventObj3.availableTickets);
