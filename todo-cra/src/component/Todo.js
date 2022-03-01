fa83597841cfa7eb4f382aeeb9743b456c15dc7b
asaudu <adaoraa.91@gmail.com>
2022-03-01 14:47:44 -0600

    Adding Todo App

---
+++ todo-cra/src/component/Todo.js
@@ -0,0 +1,18 @@
+const Todo = ({ todo, toggleTodo, index, deleteTodo }) => {
+    return (
+      <div>
+        <label htmlFor={`todo-${index}`}>
+          <input
+            id={`todo-${index}`}
+            type="checkbox"
+            checked={todo.isCompleted}
+            onChange={() => toggleTodo(index)}
+          />
+          {todo.text}
+          <button onClick={() => deleteTodo(index)}>delete</button>
+        </label>
+      </div>
+    );
+  };
+  
+  export default Todo;
