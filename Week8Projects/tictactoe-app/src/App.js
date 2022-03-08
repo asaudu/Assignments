0988dfa9003fa488aa8028035745a2ceaba900e7
asaudu <adaoraa.91@gmail.com>
2022-03-07 20:56:33 -0600

    game finished

--- Week8Projects/tictactoe-app/src/App.js
+++ Week8Projects/tictactoe-app/src/App.js
@@ -13,15 +13,35 @@ function App() {
     let strings = Array.from(gameState);
     strings[index] = isXturn ? "X" : "O";
     updateGameState(strings);
-    updateisXturn(!isXturn)
+    updateisXturn(!isXturn);
   }
 
   useEffect(() => {
-
+    const winner = checkWinner();
+    if(winner) {
+      alert(`Oh snap! ${winner} done won the game!`)
+      updateGameState(initialState)
+    }
   }, [gameState])
 
   const checkWinner = () => {
-    
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+        return gameState[a];
+      }
+    }
+    return null;
   }
 
   return (
@@ -44,7 +64,7 @@ function App() {
         <Square className="b-right" state={gameState[7]} onClick={() => onSquareClicked(7)}/>
         <Square state={gameState[8]} onClick={() => onSquareClicked(8)}/>
       </div>
-      <button className="clear-button">Game Reset</button>
+      <button className="clear-button" onClick={() => updateGameState(initialState)}>Game Reset</button>
       <p>Games with Ad™</p>
     </div>
   );
