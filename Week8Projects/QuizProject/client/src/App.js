d867608fc6adc8e03a9a2af23d114a7bbf7a5c66
asaudu <adaoraa.91@gmail.com>
2022-03-09 15:03:59 -0600

    no quiz but almost

--- Week8Projects/QuizProject/client/src/App.js
+++ Week8Projects/QuizProject/client/src/App.js
@@ -4,25 +4,31 @@ import './App.css';
 import Quiz from './Component/Quiz';
 
 function App() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState();
   const [currentUrl, setCurrentUrl] = useState('/api')
 
   useEffect(() => {
     fetch(currentUrl)
-      .then((res) => res.json())
-      .then((res) => setData(data));
+      .then((res) => {
+        console.log("data line 13", data)
+        return res.json()
+      })
+      .then((resJson) => {
+        console.log("line 14 data ", resJson)
+       return setData(resJson)
+      });
   }, [currentUrl]);
-console.log(data.results + " hello")
+console.log("the data ", data)
   return (
     <div className="App">
       <h1>React Website with an Express backend</h1>
 
       {/* <button onClick={connectToBackend}>Send Request to Backend</button> */}
       {/* Render the newly fetched data inside data */}
-      {/* <p>{!data ? "Loading Love..." : ''}</p>
-      {data && data.results[0].map((animeQuiz) => {
+      {/* <p>{!data ? "Loading Love..." : ''}</p> */}
+      {data && data.results.map((animeQuiz) => {
         return <Quiz lequiz={animeQuiz}/>
-      })} */}
+      })}
       {/* <p>{data}</p> */}
     </div>
   );
--- Week8Projects/QuizProject/client/src/Component/Quiz.js
+++ Week8Projects/QuizProject/client/src/Component/Quiz.js
@@ -2,6 +2,7 @@ const Quiz = ({lequiz}, props) => {
 
     return(
         <div>
+            Quiz page here
             <h2>{lequiz.results}</h2>
         </div>
     )
