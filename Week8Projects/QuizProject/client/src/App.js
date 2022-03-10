4bfed8238df3d0995da166eef7406f42672a782f
asaudu <adaoraa.91@gmail.com>
2022-03-10 15:54:16 -0600

    working!

--- Week8Projects/QuizProject/client/src/App.js
+++ Week8Projects/QuizProject/client/src/App.js
@@ -6,6 +6,10 @@ import Quiz from './Component/Quiz';
 function App() {
   //keep useState undefined so as not to interupt processes moving forward
   const [data, setData] = useState();
+  //useState for Current Question set to 0 so it starts from the beginning
+  const [currQ, setCurrQ] = useState(0);
+  //this useState keeps the score and is also set to 0
+  const [score, setScore] = useState(0);
   //line below was implemented when I got help from a fellow participant since I mentioned my quiz renders new questions each time. Not sure this is needed though
   //const [currentUrl, setCurrentUrl] = useState('/api')
 
@@ -23,18 +27,37 @@ function App() {
   }, []);
   //checking that the data is actually being received.
 console.log("the data ", data)
+
+if (!data) return <div>loading....</div>;
+const onAnswerClick = (answer) => {
+  if(answer === data.results[currQ].correct_answer) {
+    setScore((prevScore) => prevScore + 1);
+  }
+  setCurrQ((prevQ) => prevQ + 1);
+};
+
   return (
     <div className="App">
-      <h1>React Website with an Express backend</h1>
+      <h1>So you say you like anime</h1>
 
-      {/* <button onClick={connectToBackend}>Send Request to Backend</button> */}
       {/* Render the newly fetched data inside data */}
       {/* will show Loading if there are any issues retrieving the data */}
       <p>{!data ? "Loading Love..." : ''}</p>
-      {data && data.results.map((animeQuiz) => {
-        return <Quiz lequiz={animeQuiz}/>
-      })}
-      {/* <p>{data}</p> */}
+      {/* below would allow for me to retrieve the data and assign each question to an index and then be called that way */}
+      {/* {data && data.results.map((animeQuiz, index) => {
+        return <Quiz key={index} lequiz={animeQuiz} onAnswerClick={onAnswerClick}/>
+      })} */}
+      {/* setting up this ternary statement instead retrieves the data a question at a time per the length of the quiz by utilizing the previously set useState */}
+      Quiz hereee
+      {currQ >= data.results.length ? (
+        <div>
+          {/* this line defines the score to be shown as what you got right over the total number of questions */}
+          Game Over Score: {score}/{data.results.length}
+        </div>
+      ) : (
+        <Quiz lequiz={data.results[currQ]} onAnswerClick={onAnswerClick} />
+      )}
+      
     </div>
   );
 }
--- Week8Projects/QuizProject/client/src/Component/Quiz.js
+++ Week8Projects/QuizProject/client/src/Component/Quiz.js
@@ -1,10 +1,19 @@
-const Quiz = ({lequiz}, props) => {
+//passing in the props and function for the button
+const Quiz = ({ lequiz, onAnswerClick }) => {
+    const { question, incorrect_answers, correct_answer } = lequiz;
 
     return(
         <div>
-            Quiz page here
-            <h2>{lequiz.results}</h2>
-            <p>{props.number}</p>
+            <h3>{question}</h3>
+            <ul>
+                {incorrect_answers.map((ans, index) => (
+                <li onClick={() => onAnswerClick(ans)} key={index}>
+                    {ans}
+                </li>
+                ))}
+                <li onClick={() => onAnswerClick(correct_answer)}>{correct_answer}</li>
+            </ul>
+            
             {/* below made everything disappear again and prevented loading plus caused more erros of an uncaught error. did not resolve unique key warning */}
             {/* <p>{props.result[0].question}</p> */}
             {/* <button>{answer and next question}</button> */}
